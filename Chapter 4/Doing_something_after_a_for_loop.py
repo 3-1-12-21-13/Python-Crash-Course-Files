9afe749d27fc0e5fdb80e8b01c02a734f3a26192
3-1-12-21-13 <muircalum3@gmail.com>
2022-05-14 16:49:50 +0100

    Created file

--- Chapter 4/Doing_something_after_a_for_loop.py
+++ Chapter 4/Doing_something_after_a_for_loop.py
@@ -1,2 +1,12 @@
 # DOING SOMETHING AFTER A FOR LOOP
-#
+# You can continue to run code after a for loop. You just have to remember not
+# to indent the code. It's important to understand that it won't apply to
+# all the items on a list like the for loop. 
+# Example below:
+invites = ['ghenghis','angela','oscar','kublai']
+for invite in invites:
+    print(f"\nDear {invite.title()}, Would you like to attend my soiree?")
+    print(f"Oh and {invite.title()}? Please bring a culturally appropriate gift.")
+print(f"\nAlso please remember everyone that {invites[-1].title()} is not \
+drinking at this time and would respect everyones discretion.\nThat means you, \
+{invites[-2].title()}.")
