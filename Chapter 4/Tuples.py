c9f08d03fdb239d3981a42dfb7f736fde113a4be
3-1-12-21-13 <muircalum3@gmail.com>
2022-05-16 22:11:43 +0100

    "Started work on Tuples on the laptop."

---
+++ Chapter 4/Tuples.py
@@ -0,0 +1,4 @@
+# TUPLES
+# Sometimes you will want to create a list of items where the values cannot
+# change. Python refers to values which cannot change as immutable. A list of
+# Immutable items is called a tuple.
--- Chapter 4/Using_the _range_function.py
+++ Chapter 4/Using_the _range_function.py
@@ -9,7 +9,4 @@ for value in range(1,5):
 # which would have been 5 in this case.
 # You can also pass range() only one argument, and it will start the sequence
 # of numbers at 0. For example, range(6) would return the numbers from 0 
-# through 5.
-
-# Adding a line of text to test git functionality
-# Adding another line to test git functionality.
+# through 5.1
