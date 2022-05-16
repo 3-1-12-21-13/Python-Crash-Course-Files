58c346284dde2ba33c58d544ce595ae30fdc8841
3-1-12-21-13 <muircalum3@gmail.com>
2022-05-16 21:43:23 +0100

    test from laptop

--- Chapter 4/Using_the _range_function.py
+++ Chapter 4/Using_the _range_function.py
@@ -10,3 +10,5 @@ for value in range(1,5):
 # You can also pass range() only one argument, and it will start the sequence
 # of numbers at 0. For example, range(6) would return the numbers from 0 
 # through 5.
+
+# Adding a line of text to test git functionality
