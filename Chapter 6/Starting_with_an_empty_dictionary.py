51eb8a005c21aab979016b45afa578aaabe3d9e2
3-1-12-21-13 <79387034+3-1-12-21-13@users.noreply.github.com>
2022-06-07 19:20:46 +0100

    Started chapter 6

---
+++ Chapter 6/Starting_with_an_empty_dictionary.py
@@ -0,0 +1,3 @@
+# STARTING WITH AN EMPTY DICTIONARY
+# To start filling an empty dictionary, define a dictionary with an empty set of braces and then add each key-value
+# pair on it's own line.
---
+++ Chapter 6/Working_with_dictionaries.py
@@ -0,0 +1,14 @@
+# WORKING WITH DICTIONARIES
+# A dictionary is a Python collection of key value pairs.
+# Each key is connected to a value and use can use a key to access the value
+# associated with that key. A Key's value can be a number, a string, a list
+# or even another dictionary.
+# In Python, a dictionary is wrapped in braces {} with a series of key value
+# pairs inside the braces, as shown below:
+dict_example = {'value 1': 1, 'value 2': 'two'}
+print(f"value 1 in the dictionary is - {dict_example['value 1']}")
+print(f"value 2 in the dictionary is - {dict_example['value 2']}")
+# Within dictionaries the key is the first part before the colon, in the example
+# above, the key is 'value 1' and 'value 2'. The value of the key-value pair
+# is whatever follows after the colon.
+# Whenever you call the key, the value will be used.
